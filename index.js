3fbf15af3a6e5c72eae475061253e1bb069a517e
Matthew Mason <matthew.mason@jumo.world>
2018-02-07 13:10:19 +0200

    Add routes for getting makes, models for a make and model by name

--- index.js
+++ index.js
@@ -17,6 +17,4 @@ app.post('/bot', (req, res) => {
     res.json()
 })
 
-app.get('/models', (req, res) => res.json(root.getModels()))
-
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
