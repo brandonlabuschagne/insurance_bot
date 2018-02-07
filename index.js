717b719c2a55ac426c7946deb96c34414bff14a9
Matthew Mason <matthew.mason@jumo.world>
2018-02-07 12:00:12 +0200

    Fix issues with circular js object

--- index.js
+++ index.js
@@ -2,24 +2,21 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const config = require('./config.json')
 const app = express()
+const expressPromise = require('express-promise')
 const Root = require('./lib/root')
 
 app.use(bodyParser.json())
+app.use(expressPromise())
 app.use(bodyParser.urlencoded({ extened: true }))
 
 const root = new Root(config['root-app-id'], config['root-app-secret'])
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/:name', (req, res) => {
-    const name = req.params.name
-    res.send(`Hello ${name}!`)
-})
-
 app.post('/bot', (req, res) => {
     res.json()
 })
 
-app.get('/models', (req, res) => res.json(root.models))
+app.get('/models', (req, res) => res.json(root.getModels()))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
